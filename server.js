86ee079b3f54c05a15e810665ac52d6cb9199d83
RuxCastillo <rubenldc1412@gmail.com>
2024-06-23 22:46:35 -0600

    empezando con ejs

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+import env from "dotenv";
+
+env.config();
+
+const app = express();
+const port = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.render("index", {title: "Home Page", message: "Welcome to EJS"})
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
