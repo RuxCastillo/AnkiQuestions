34ec347125fb57c7b93c7029d45e1fdc3b853d14
RuxCastillo <rubenldc1412@gmail.com>
2024-07-01 23:03:14 -0600

    haciendo pruebas con vercel

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import axios from "axios";
 env.config();
 
 const app = express();
-const port = process.env.PORT_SERVER || 3002;
+const port = process.env.PORT_SERVER || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const API_URL = "http://localhost:4000";
